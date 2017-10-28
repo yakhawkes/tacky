c37fd1dc6897deb938f7697c44244843b5ada4a3
shawkes <shawkes@gmail.com>
2017-10-28 20:54:10 +0100

    Useage class

---
+++ Useage.py
@@ -0,0 +1,12 @@
+# Import the modules
+import requests
+import json
+
+
+
+class Useage:
+
+    def  current(selfself):
+        request =  requests.get("http://www.reddit.com/user/spilcm/about/.json")
+        data = json.loads(r.text)
+        return data.speed
