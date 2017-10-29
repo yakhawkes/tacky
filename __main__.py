419431f5e9bb0ae6c2a6f5384ee574497e2ca5c5
james-dewes <shortcodecypher@gmail.com>
2017-10-29 09:45:45 +0000

    back

--- __main__.py
+++ __main__.py
@@ -65,6 +65,6 @@ button4.when_pressed = reverse
 
 while True:
     t1 = Thread(target=playSound,args={speed})
-    t2 = Thread(target=motor.makeStep,kwargs={'directon': direction,'speed': speed})
+    t2 = Thread(target=motor.makeStep,args={directon,speed})
     t1.start()
     t2.start()
