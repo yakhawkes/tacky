e91c7a243fcb4119c20d66357958c77891e07ef6
james-dewes <shortcodecypher@gmail.com>
2017-10-29 12:03:37 +0000

    indent

--- Useage.py
+++ Useage.py
@@ -5,8 +5,8 @@ import json
 
 class Useage:
     def __init__():
-    request =  requests.get("http://tackapi22.azurewebsites.net/api/useage")
-    self.data = json.loads(request.text)
+        request =  requests.get("http://tackapi22.azurewebsites.net/api/useage")
+        self.data = json.loads(request.text)
 
     def  current(self):
         self.data["speed"]
