cb0d36b4f3e5390b93b652271b9cfc1520a4cc26
james-dewes <shortcodecypher@gmail.com>
2017-10-29 08:51:09 +0000

    -

--- Motor.py
+++ Motor.py
@@ -21,6 +21,6 @@ class Motor(object):
                 self.GPIO.output(pinNumber, False)
 
         print(self.step)
-        print(direction)
+        #print(direction)
         self.step += direction
         time.sleep(speed)
